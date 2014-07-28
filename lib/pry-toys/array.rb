a61fb523fb7a5a426aad1ce1546e68836075a325
krissi <c.haase+github@ifu.com>
2014-07-28 13:30:00 +0200

    fixed documentation

--- lib/pry-toys/array.rb
+++ lib/pry-toys/array.rb
@@ -9,27 +9,27 @@ Array.class_eval do
   #
   # == Size
   #
-  # String.toy(3) # => [1, 2, 3]
+  # Array.toy(3) # => [1, 2, 3]
   #
   # == Size & block
   #
-  # String.toy(3) {|i| i + 3} # => [3, 6, 9]
+  # Array.toy(3) {|i| i + 3} # => [3, 6, 9]
   #
-  # == Size & one of the pre-determined objects, such as
+  # == Size & one of the pre-determined objects; such as
   # Integer (default), Float, String, Time, Date
   #
-  # String.toy(3, Float) # => [1.0, 2.0, 3.0]
-  # String.toy(3, String) # => ['tt', 'uuuu', 'cc']
-  # String.toy(3, Time) # => [2014-02-22 08:49:49 -0800,
+  # Array.toy(3, Float) # => [1.0, 2.0, 3.0]
+  # Array.toy(3, Array) # => ['tt', 'uuuu', 'cc']
+  # Array.toy(3, Time)  # => [2014-02-22 08:49:49 -0800,
   #                           2014-02-23 08:49:49 -0800,
   #                           2014-02-24 08:49:49 -0800]
-  # String.toy(3, Date) # => [#<Date: 2014-02-22 ((2456711j,0s,0n),+0s,2299161j)>,
+  # Array.toy(3, Date)  # => [#<Date: 2014-02-22 ((2456711j,0s,0n),+0s,2299161j)>,
   #                           #<Date: 2014-02-23 ((2456712j,0s,0n),+0s,2299161j)>,
   #                           #<Date: 2014-02-24 ((2456713j,0s,0n),+0s,2299161j)>]
   # 
   # == Size & an unknown object (that responds to new)
   #
-  # String.toy(3, Object) # => [#<Object:0x007fa36e9b9128>,
+  # Array.toy(3, Object)  # => [#<Object:0x007fa36e9b9128>,
   #                             #<Object:0x007fa36e9b9100>,
   #                             #<Object:0x007fa36e9b90d8>]
 
